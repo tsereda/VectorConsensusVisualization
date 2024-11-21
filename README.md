fe9b9b1b617c86173f04d442323fe85819a5cfae
tsereda <timgsereda@gmail.com>
2024-11-21 00:21:46 -0600

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### install bun
+curl -fsSL https://bun.sh/install | bash
+### install d3
+bun install d3
+### start server
+bun run dev
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import ForceGraph from '$lib/components/ForceGraph.svelte';
+  
+    const graphData = {
+      nodes: [
+        { id: "A", group: 1 },
+        { id: "B", group: 1 },
+        { id: "C", group: 2 },
+        { id: "D", group: 2 }
+      ],
+      links: [
+        { source: "A", target: "B", value: 1 },
+        { source: "B", target: "C", value: 2 },
+        { source: "C", target: "D", value: 1 },
+        { source: "D", target: "A", value: 3 }
+      ]
+    };
+  </script>
+  
+  <ForceGraph data={graphData} />
